42be8aa5c3463e10a8049e4e6e648c82a621cb4e
EC2 Default User <ec2-user@ip-172-31-44-10.us-east-2.compute.internal>
2019-02-20 13:32:47 +0000

    ボタン遷移

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'address/show'
   root to: 'address#show'
   
-  get 'address/detail'
+  get 'detail/detail'
+  # get 'detail/detail/:id'=> 'detail#detail'
+  # => 'detail#detail',as:'detail_detail'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
